99e7ecbcf57472ef4b1c9499b9cfdcfa4a2e2f82
Jorge Vilca <jlvilcav@gmail.com>
2022-07-28 09:17:25 -0500

    Creando un httpService generico

--- src/app/services/base-http.services.ts
+++ src/app/services/base-http.services.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
+import { UpdateProductDto } from "../dtos/product.dto";
 import { Product } from "../models/product.model";
 import { Category } from "./../models/category.model";
 
 export class BaseHttpService<TypeClass> {
   // data: TypeClass[] = [];
-  constructor(private url: string) {}
+  constructor(protected url: string) {}
 
   async getAll() {
     const {data} = await axios.get<TypeClass[]>(this.url);
     return data;
+  }
+
+  async update<ID,DTO>(id: ID, changes: DTO) {
+    const {data} = await axios.put(`${this.url}/${id}`, changes);
+    return data;
+    // throw new Error("Method not implemented.");
   }
 }
 
@@ -20,14 +27,20 @@ export class BaseHttpService<TypeClass> {
 
 (async () => {
   const url1 = 'https://api.escuelajs.co/api/v1/products';
-  const service = new BaseHttpService<Product>(url1);
-  const products = await service.getAll();
-  console.log('Products ',products.length);
+  const productService = new BaseHttpService<Product>(url1);
+
+  const rta = await productService.getAll();
+  console.log('Products ',rta.length);
+  productService.update<Product['id'], UpdateProductDto>(1, {
+    title: 'Producto 1 actualizado'
+  });
 
   const url2 = 'https://api.escuelajs.co/api/v1/categories';
   const categoryService = new BaseHttpService<Category>(url2);
-  const rta = await categoryService.getAll();
-  console.log('Categories ',rta.length);
+  // categoryService.
+
+  const rta1 = await categoryService.getAll();
+  console.log('Categories ',rta1.length);
 
 })();
 // const url1 = 'https://api.escuelajs.co/api/v1/products';
---
+++ src/app/services/product-crud.service.ts
@@ -0,0 +1,17 @@
+import { Product } from "../models/product.model";
+import { BaseHttpService } from "./base-http.services";
+import { UpdateProductDto } from "../dtos/product.dto";
+import { ProductHttpService } from "./product-http2.service";
+
+export class ProductCRUDService {
+  private url = 'https://api.escuelajs.co/api/v1/products';
+
+  private http = new ProductHttpService(this.url);
+
+  async update(id: Product['id'], dto: UpdateProductDto) {
+    this.http.otroRequest();
+    const {data} = await this.http.update(id, dto);
+
+    return data;
+  }
+}
