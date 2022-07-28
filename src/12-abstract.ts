8c62f3766b30270af7a7bcdfbdcb951cb26b70df
Jorge Vilca <jlvilcav@gmail.com>
2022-07-27 19:01:52 -0500

    Clases abstractas

---
+++ src/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal, Dog } from './09-protected';
+
+// const animal = new Animal('elite');
+// animal.greeting();
+
+const cheis = new Dog('cheis', 'Jorge');
+cheis.greeting();
+cheis.woof(1);
